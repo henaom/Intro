7fa9671109b5b514a5eb5e20afb030864918c3ed
henaom <160857383+henaom@users.noreply.github.com>
2024-02-22 07:57:01 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -16,4 +16,24 @@ st.subheader("Ahora usemos 2 columnas")
 
 col1, col2 = st.columns(2)
 
+with col1:
+         st.subheader("Esta es la primera columna")
+         st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+         resp = st.checkbox ("Estoy de acuerdo")
+         if resp:
+                  st.write("Correcto !")
+                  
+with col2:
+         st.subheader("Esta es la segunda columna")
+         modo = st.radio("Que Modalidad es la principal en tu interfaz", ("Visual", "Auditiva", "Tácti"))
+         if modo == "Visual":
+                  st.write("La vista es fundamental para tu interfaz")
+         if modo == "Auditiva":
+                  st.write("La audicion es fundamental para tu interfaz")
+         if modo == "Tácti" :
+                  st.write("El tacto es fundamental para tu inferfaz")
+                  
+                  
+         
+                         
 
