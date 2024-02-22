cb5bcfed02b3f29a362708bc78e90490aa3b4341
henaom <160857383+henaom@users.noreply.github.com>
2024-02-22 07:29:21 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -6,3 +6,6 @@ st.title(" Bienvenido moooorchi")
 st.header("En este espacio vamos a crear solo cosas lindas")
 st.write("Estas preparado para soñar?")
 image=Image.open("morchis1.jpeg")
+
+st.image(image, caption="Interfaces Multimodales")
+
