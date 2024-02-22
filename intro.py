2a4392c2552a00c228c9bb4f44c0e24b624dd512
henaom <160857383+henaom@users.noreply.github.com>
2024-02-22 07:36:21 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -7,5 +7,12 @@ st.header("En este espacio vamos a crear solo cosas lindas")
 st.write("Estas preparado para soñar?")
 image=Image.open("morchis1.jpeg")
 
-st.image(image, caption="Interfaces Multimodales")
+st.image(image, caption="Lo que vamos a soñar")
+
+text = st.text_input("Escribe algo, "Este es mi texto")
+st.write("El texto escrito es", texto)
+
+st.subheander("Ahora usemos 2 columbas")
+
+col1, col2 = st.columnas(2)
 
