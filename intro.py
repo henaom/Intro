d3f6111e5282d07caf1b2ed987c029c663c057db
henaom <160857383+henaom@users.noreply.github.com>
2024-02-22 07:22:39 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -5,4 +5,4 @@ st.title(" Bienvenido moooorchi")
          
 st.header("En este espacio vamos a crear solo cosas lindas")
 st.write("Estas preparado para soñar?")
-imagen.=Imagen.open("morchis.1")
+imagen.=Image.open("morchis.1")
